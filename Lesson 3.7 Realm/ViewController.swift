50b993e491627782cf7b994f809a8d569f6a4ccf
Artem Potemkin <art.potemkin@gmail.com>
2023-11-27 14:28:38 +0300

    Initial Commit

---
+++ Lesson 3.7 Realm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson 3.7 Realm
+//
+//  Created by Артём Потёмкин on 27.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
